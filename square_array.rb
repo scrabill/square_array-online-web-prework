6ba5ff16fbda321bc88541305de384bdbd294dbf
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-06-03 15:41:41 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,14 @@
 def square_array(array)
   # your code here
+
+  new_numbers = []
+
+  array.each do |array|
+  new_numbers.push(array * array)
+  # puts array * array
+
+ end
+ return new_numbers
 end
+
+square_array([1,2,3])
